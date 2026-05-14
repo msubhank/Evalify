b0588826188cba595eea926182ede00e2041286c
msubhank <msubhank123@gmail.com>
2026-05-14 13:05:42 +0500

    Security fix

--- evalify-frontend/services/supabase.js
+++ evalify-frontend/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://adadhbjetldculujncut.supabase.co';
-const supabaseKey = 'sb_publishable_cgALGONTL2FrT_9uPcfmTQ_2CFVj87K';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
